f7614ef1af4a763487612fdc279d1f0d6f1faed1
yassin <yassinam.pro@gmail.com>
2024-08-23 17:05:41 +0200

    fix: layout error fixed

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,9 +4,9 @@ import { Suspense } from "react";
 
 export default function AuthLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <Suspense fallback={<CustomLoading />}>
       <DisconnectedProvider>{children}</DisconnectedProvider>
