9fd7666a291effe94514446acf650d44df6f7274
Yassin <55637659+IIyn@users.noreply.github.com>
2024-08-23 17:09:43 +0200

    Create docker-image.yml (#3)
    * Create docker-image.yml
    
    * fix: layout error fixed
    
    * fix: import error

--- src/app/(connected)/layout.tsx
+++ src/app/(connected)/layout.tsx
@@ -4,9 +4,9 @@ import { Suspense } from "react";
 
 export default function ConnectedLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>)
+}) {
   return (
     <Suspense fallback={<CustomLoading />}>
       <ConnectedProvider>{children}</ConnectedProvider>
